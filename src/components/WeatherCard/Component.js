5b83dc8abd1246ca09c145c913962fb9b7106bfb
Sebastiaan <scrisan@happy-to-serve.com>
2020-08-24 18:52:47 +0200

    change card color dual gradient

--- src/components/WeatherCard/Component.js
+++ src/components/WeatherCard/Component.js
@@ -7,7 +7,14 @@ import Condition from "./Condition";
 const WeatherCard = (props) => {
   const Card = styled.div`
     margin: 0 auto;
-    background-image: linear-gradient(to bottom, red, pink);
+    // background-image: linear-gradient(-40 deg, red, white);
+    background: -webkit-linear-gradient(
+      top,
+      #ffb76b 0%,
+      #ffa73d 80%,
+      #ff7c00 81%,
+      #ff7f04 100%
+    );
     width: 200px;
     height: 240px;
     display: flex;
